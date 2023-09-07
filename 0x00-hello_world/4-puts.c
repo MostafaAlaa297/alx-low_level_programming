27910f024240fb7d955e0d403fcf5440f7e81178
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-07 10:13:41 -0700

    create C program to print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+
+	return 0;
+}
