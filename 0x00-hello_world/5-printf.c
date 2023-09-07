ce752938d346b6ad0c83101e9c26b4b5d0d285ba
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-07 10:23:55 -0700

    create C program to print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
