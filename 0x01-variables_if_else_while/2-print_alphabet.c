54a72615160ef01ec1c48ff8f9199d2f38b8118e
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 10:33:19 -0700

    make a program that prints alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char alphabets[] = 
-	{
+	char alphabets[] = {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 		's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'
 	};
 
-	long unsigned int i;
+	unsigned long int i;
 	for (i = 0; i < sizeof(alphabets); i++)
 	{
 		putchar(alphabets[i]);
