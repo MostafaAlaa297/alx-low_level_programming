dc0564e16a01565808fc964f07164b747cdb64dc
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 14:44:27 -0700

    program prints hexadecimals in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	char hexadecimals[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-	int i;
+
+	unsigned int i;
 
 	for (i = 0; i < sizeof(hexadecimals); i++)
 	{
