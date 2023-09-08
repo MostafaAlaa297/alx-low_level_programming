306d8594f27f94c452ad182b9dfc152586df6f83
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 14:48:20 -0700

    edits for betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-	char hexadecimals[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	char hexadecimals[] = {
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'a', 'b', 'c', 'd', 'e', 'f'
+	};
 
 	unsigned int i;
 
