19263dbf2da6be278b7606c90b2828a48edd051e
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 14:22:23 -0700

    program prints alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	for (i = sizeof(alphabets)-1; i <= 0; i--)
+	{
+		putchar(alphabets[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
