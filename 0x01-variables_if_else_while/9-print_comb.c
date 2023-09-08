6b595b195d2c5335ea3fa55fc6c6ecd9f90f721e
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 15:26:38 -0700

    program that prints single digits separated by ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
+
+	putchar(9 + '0');
 
 	return (0);
 }
