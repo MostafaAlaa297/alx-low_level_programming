b8912a7bb1be2fa0cc7ceece8804bc378d3c8cc2
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 12:15:29 -0700

    program to print base 10 nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	unsigned long i;
+
+	for (i = 0; i < 10; i++)
+	{
+		puts(i);
+	}
+
+	return (0);
+}
