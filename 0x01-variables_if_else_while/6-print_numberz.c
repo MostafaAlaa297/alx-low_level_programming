e16883b79162ac0037151e03fb1626e8ce99ba9d
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-08 12:37:41 -0700

    program prints base 10 nums with newline

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	unsigned int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	
+	
+	return (0);
+}
