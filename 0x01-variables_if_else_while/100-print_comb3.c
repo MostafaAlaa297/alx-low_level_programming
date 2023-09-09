c13e47239b16ac5ab14e6f63674cae5cbd243790
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-09 14:36:59 -0700

    fix formatting issues for betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
         {
 		for(j = i + 1; j < 10; j++)
 		{
-                	putchar(i + '0');
+			putchar(i + '0');
 			putchar(j + '0');
 			if (i == 8 && j == 9)
 				break;
@@ -23,6 +23,6 @@ int main(void)
         }
 
 	putchar('\n');
-
+	
 	return (0);
 }
