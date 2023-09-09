dc344a486527910321be73613b69c4c44e52f1c0
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-09 14:33:16 -0700

    print all different combinations of two digit nums

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
         {
-		int j;
-
-		for(j = 1; j < 10; j++)
+		for(j = i + 1; j < 10; j++)
 		{
                 	putchar(i + '0');
 			putchar(j + '0');
