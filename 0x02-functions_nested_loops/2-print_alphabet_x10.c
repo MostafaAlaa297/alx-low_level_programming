e91b12b5e4828afca067b8e7f6e016646b583170
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 16:17:06 -0700

    Print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,19 @@
  */
 void print_alphabet_x10(void)
 {
+	/* declare local variables*/
 	char c;
 	int i;
-	
+	/* nested for loop and this is for number of times */
 	for (i = 0; i < 10; i++)
 	{
+		/* loop for alphabets */
 		for (c = 'a'; c <= 'z'; c++)
 		{
+			/* print each alphabet */
 			_putchar(c);
 		}
+		/* print newline each time  */
 		_putchar('\n');
 	}
 
