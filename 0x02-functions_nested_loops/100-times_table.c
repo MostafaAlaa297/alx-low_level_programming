fca9d36688501152bba6b0fdcef9c48ea0d53704
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-13 08:05:42 -0700

    Edits and fixes for betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,34 @@
 #include "main.h"
 
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: integer to start printing from
+ *
+ * Return: void
+ */
+
 void print_times_table(int n)
 {
-  int i, j;
-
-  if (n < 0 || n > 15) {
-    return;
-  }
-
-  for (i = 0; i <= n; i++) {
-    for (j = 0; j <= n; j++) {
-      _putchar((i * j) + '0');
-      if (j < n) {
-        _putchar(',');
-        _putchar(' ');
-      }
-    }
-    _putchar('\n');
-  }
+	/* Local variables */
+	int i, j;
+	
+	/* conditionals to print from 0 to 15 */
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	/* nested loops to print records and fields*/
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar((i * j) + '0');
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
