c022a79b45b1a8b9277d6def6223d68d10690df0
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 17:12:38 -0700

    fix style issues for betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  */
 int _isalpha(int c)
 {
-        /* If conditional to check for letters */
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-                return (1);
-        else
-                return (0);
+	/* If conditional to check for letters */
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
