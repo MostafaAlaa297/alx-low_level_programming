2232185ce4e46aeb2a6d896b94e8951da7560fb2
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 20:35:25 -0700

    Print natural nums starting from n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,33 @@
 #include "main.h"
 
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: Starting number
+ */
 void print_to_98(int n)
 {
-	int i;
-	
-	for (i = n; i < 98; i++)
+	if (n <= 98)
 	{
-		if (i < 0)
+		for (; n < 98; n++)
 		{
-			_putchar('-');
-			i = -i;
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
-		if (i < 10)
-			_putchar(i + '0');
-		else
+	}
+	else
+	{
+		for (; n > 98; n--)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-		}
-		
-		if (i != 98)
-		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
 	
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
 	_putchar('\n');
 }
