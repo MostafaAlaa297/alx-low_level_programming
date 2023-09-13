355b99a8636974d48be626b85ab1a9cf53193724
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 17:10:12 -0700

    Check if char is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks if a character is a letter
  * @c: The character to be checked
- *
- * Return: 1 if c is lowercase, 0 otherwise
+ * 
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	/* If conditional to check for lower and capital */
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+        /* If conditional to check for letters */
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                return (1);
+        else
+                return (0);
 }
