b9cff6813ebf7d5a7b136016eddb4cd8eeac6707
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 19:00:07 -0700

    Fix code printing more spaces than required

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,24 +12,31 @@ void times_table(void)
 	int i, j, product;
 
 	/* Nested for loops to print 9 times table */
-	for (i = 0; i <= 9; i++) {
-		for (j = 0; j <= 9; j++) {
+	for (i = 0; i <= 9; i++) 
+	{
+		for (j = 0; j <= 9; j++) 
+		{
 			/* Calculate product */
 			product = i * j;
 
 			/* Prints table nums */
-			if (product < 10) {
-				if (j != 0) {
+			if (product < 10) 
+			{
+				if (j != 0) 
+				{
 					_putchar(' ');
 				}
 				_putchar(product + '0');
-			} else {
+			} 
+			else 
+			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
 
 			/* Prints comma to separate nums */
-			if (j < 9) {
+			if (j < 9) 
+			{
 				_putchar(',');
 				_putchar(' ');
 			}
