aa90f6e23dafaa6bf6eddc9760cb72ed660fb6fa
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-12 18:49:50 -0700

    Fix code not printing two digits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,21 +8,37 @@
  */
 void times_table(void)
 {
-	/* Local vars to store numbers*/
-	int i, j;
+	/* Local vars to store numbers */
+	int i, j, product;
 
 	/* Nested for loops to print 9 times table */
-	for (i = 0; i <= 9; i++)
-	{
-		for(j = 0; j <= 9; j++)
-		{
-			_putchar((i * j) + '0'); /* Prints table nums */
-			if(j < 9)
-			{
-				_putchar(','); /* Prints comma to seperate nums */
-				_putchar(' '); /* Prints space after comma */
+	for (i = 0; i <= 9; i++) {
+		for (j = 0; j <= 9; j++) {
+			/* Calculate product */
+			product = i * j;
+
+			/* Prints table nums */
+			if (product < 10) {
+				_putchar(product + '0');
+			} else {
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			/* Prints comma to seperate nums */
+			if (j < 9) {
+				_putchar(',');
+
+				/* Prints space after comma */
+				_putchar(' ');
+				if (product < 10) {
+					_putchar(' '); /* extra space for single digit numbers */
+				}
 			}
 		}
-		_putchar('\n'); /* Prints newline */
+
+		/* Prints newline */
+		_putchar('\n');
 	}
 }
+
