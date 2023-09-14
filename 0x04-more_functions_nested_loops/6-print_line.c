5bc276fa170c9c8965a89d962fdc99312ad76df5
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-14 16:55:10 -0700

    Draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,14 @@
  */
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
         {
 		_putchar('\n');
 		return;
         }
 
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		_putchar('_');
+	
 	_putchar('\n');
 }
