b0c3bd8487921a9e22784e134e86c04017a2e4fd
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-14 17:58:01 -0700

    Fixes for betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,17 +8,17 @@
  */
 void print_square(int size)
 {       
-        int i, j;
+	int i, j;
 
-        if (size <= 0)
-        {
-                _putchar('\n');
-                return;
-        }
-        
-        for (i = 0; i < size; i++)
-        {       
-                for(j = 0; j < size; j++)
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
