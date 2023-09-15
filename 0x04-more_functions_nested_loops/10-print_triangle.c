3d1b8c2c57d2021a3c2b2b6c33cbe87b3532f417
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-15 05:55:57 -0700

    Enhance trangle drawing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,7 +6,7 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -14,12 +14,17 @@ void print_triangle(int size)
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
 		{
 			_putchar('#');
 		}
+
 		_putchar('\n');
 	}
 }
