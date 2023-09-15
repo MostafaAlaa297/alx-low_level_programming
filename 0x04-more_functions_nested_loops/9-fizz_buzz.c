e86d183b2fa69c7e6a94d1890ac045b1007612de
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-15 05:13:33 -0700

    Fix fizz buzz issues

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
+#include "main.h"
 
-int main() {
-	for (int i = 1; i <= 100; i++)
+/**
+ *
+ * fizz_buzz - Prints fizz for the multiples of 3 and buzz for 5 followed by a new line
+ *
+ * Return: void
+ */
+
+void fizz_buzz(void)
+{
+	/* Local variable */
+	int i;
+
+	/* For loop to check for 3 and 5 multiples */
+	for (i = 1; i <= 100; i++)
 	{
-    		if (i % 3 == 0 && i % 5 == 0)
+		/* Conditionals to check for 3 and 5 multiples cases */
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-      			printf("FizzBuzz ");
+			printf("FizzBuzz "); /* Prints FizzBuzz when there are 3 and 5 multiples  */
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz "); /* Prints Fizz when there is a 3 multiple */
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz "); /* Prints Buzz when there is a 5 multiple */
 		}
 		else
 		{
-			printf("%d ", i);
+			_putchar(i + '0'); /* Prints the rest of the numbers */
 		}
 	}
-
-	printf("\n");
-	return (0);
+	
+	_putchar('\n'); /* Prints a newline */
 }
