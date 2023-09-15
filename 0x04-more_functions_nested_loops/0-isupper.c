19f0453d58f91f5ef582918a58e9808b1dc82b59
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-15 04:52:24 -0700

    Try to fix issues for betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
+
 /**
  * isupper - checks for upper case chars
  *
  * Return: 0 if uppercase and 1 if not
  */
+
 int _isupper(int c)
 {
+	/* Conditional to check for uppercase */
 	if (c >= 'A' && c <= 'Z')
-		return (1);
+		return (1); /* Return 1 if the char is uppercase */
 	else
-		return (0);
+		return (0); /* Return 0 otherwise */
 }
