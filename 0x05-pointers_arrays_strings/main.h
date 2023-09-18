db1fce8fb5721a4ebdfaf29732b1d50214263461
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-18 16:45:37 -0700

    Fix issues

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void rev_string(char *s);
-
-void print_rev(char *s);
-
 void _puts(char *str);
 
 int _strlen(char *s);
@@ -14,5 +10,9 @@ void swap_int(int *a, int *b);
 int _putchar(char c);
 
 void reset_to_98(int * n);
+
+void rev_string(char *s);
+
+void print_rev(char *s);
 
 #endif /* MAIN_H */
