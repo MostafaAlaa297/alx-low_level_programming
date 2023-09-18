fbe6358f3f933906bc27d8370a8770adea1c5b31
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-18 16:52:35 -0700

    Fix _strlen reference issue

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,3 +18,19 @@ void rev_string(char *s)
 		s[j] = temp;
 	}
 }
+
+/**
+ * _strlen - returns length of a string
+ * @s: the string to calculate its length
+ *
+ * Return: number of chars in a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+
+	return (i);
+}
