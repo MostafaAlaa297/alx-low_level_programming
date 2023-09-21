361c8adc3a22e46ce4f94a78c0f52ae9e070997e
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-21 02:43:44 -0700

    Concatenate two strings with n var that limits the num of printed chars

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+/**
+ * strncat - Concatenates two strings
+ * but with specified number of bytes of the second string
+ * @dest: The string to concatenate to
+ * @src: The string to concatenate
+ * @n: The number of bytes to use from the second string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src && n > 0)
+	{
+		*dest = *src;
+		
+		dest++;
+		src++;
+		n--;
+	}
+
+        *dest = '\0';
+
+	return (start);
+
+}
