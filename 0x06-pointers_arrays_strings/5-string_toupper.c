6ecd570d41fa2c1c6599216b4d682268b6e4cb8a
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-21 13:57:00 -0700

    Change string letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @*str: A pointer that points at the first char of a string
+ *
+ * Return: A string of uppercase letters
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return str;
+}
