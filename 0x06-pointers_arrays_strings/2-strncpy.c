400e8dbd74ba04ac5398ffd3315137bfbad784fc
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-21 07:39:33 -0700

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+/**
+ * strncpy - copies a string
+ * @dest: string to copy to
+ * @src: string to copy
+ * @n: number of cahrs to copy
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}
