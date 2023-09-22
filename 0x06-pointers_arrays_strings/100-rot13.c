c1e716d7c5f97072451fb07dfa838e6153a7813c
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-22 12:00:02 -0700

    Encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * rot13 - Replaces each letter in a string with the 13th letter
+ * after it in the latin alphabet
+ * @str: The string to encode using rot13
+ *
+ * Return: The encoded string
+ */
+char *rot13(char *str)
+{
+	char *start = str;
+	
+	char *rot13 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *encoded = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; rot13[j] != '\0'; j++)
+		{
+			if (str[i] == rot13[j])
+			{
+				str[i] = encoded[j];
+				break;
+			}
+		}
+	}
+	
+	return (start);
+}
