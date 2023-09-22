6c0e17c459db2d41592889f6d44ad702ff4d95bc
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-22 08:18:16 -0700

    Fix issues for betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,8 @@
+#include <ctype.h>
+
 /**
- * Capitalizes all words of a string
- * @str: the string to capitalize its words
+ * is_sep - Checks for seperators
+ * @c: The character to check for
  *
  * Return: The string with each word capitalized
  */
@@ -12,25 +14,31 @@ int is_sep(char c)
 	for (i = 0; seperators[i] != '\0'; i++)
 	{
 		if (c == seperators[i])
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
-char * cap_string(char *str)
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: the string to capitalize its words
+ *
+ * Return: The string with each word capitalized
+ */
+char *cap_string(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] != ' ')
-			str[i] -= 32;
+			str[i] = toupper(str[i]);
 		if (is_sep(str[i]))
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] -= 32;
+				str[i + 1] = toupper(str[i + 1]);
 		}
 	}
 
-	return str;
+	return (str);
 }
