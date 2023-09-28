ed5455a92749d0d0348dc884f9bdefe1745e3deb
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-28 15:49:29 -0700

    Fix for betty style and doc

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,8 @@
 /**
- * is_prime_number - Checks if an integer is a prime number
+ * check_prime - Checks if an integer is a prime number
  * @n: The integer to be checked
+ * @divisor: The number to divide n with to
+ * test if n is prime or not
  *
  * Return: 1 if n is a prime number, 0 otherwise
  */
@@ -8,13 +10,26 @@
 int check_prime(int n, int divisor)
 {
 	if (divisor == 1)
-		return (1); /* n is prime because it's only divisible by 1 and itself */
+	{
+		/* n is prime because it's only divisible by 1 and itself */
+		return (1);
+	}
 
 	if (n % divisor == 0)
-		return (0); /* n is not prime because it's divisible by a number other than 1 and itself */
+	{
+		/* n is not prime because it's divisible by a number other than 1 and itself */
+		return (0);
+	}
 
-	return check_prime(n, divisor - 1);
+	return (check_prime(n, divisor - 1));
 }
+
+/**
+ * is_prime_number - Checks if an integer is a prime number
+ * @n: The integer to be checked
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
 
 int is_prime_number(int n)
 {
