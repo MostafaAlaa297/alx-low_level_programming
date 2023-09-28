912e476ce15ba37d0f483d518967a6d39aa9ea0a
MostafaAlaa297 <elaskary609@gmail.com>
2023-09-28 15:33:38 -0700

    Fix for betty style and doc

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 /**
- * _sqrt_recursion - Returns the natural square root of a number
+ * calculate_sqrt - Returns the calculation of the
+ * natural square root of a number
  * @n: The number to get its square root
+ * @guess: The guess of the square root of n
  *
  * Return: If n does not have a natural square root,
  * the function should return -1, otherwise,
@@ -15,8 +17,17 @@ int calculate_sqrt(int n, int guess)
 	if (guess * guess > n)
 		return (-1); /* n doesn't have a natural square root */
 
-	return calculate_sqrt(n, guess + 1);
+	return (calculate_sqrt(n, guess + 1));
 }
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number to get its square root
+ *
+ * Return: If n does not have a natural square root,
+ * the function should return -1, otherwise,
+ * it returns the square root
+ */
 
 int _sqrt_recursion(int n)
 {
@@ -26,5 +37,5 @@ int _sqrt_recursion(int n)
 	if (n == 0 || n == 1)
 		return (n); /* Square root of 0 is 0, square root of 1 is 1 */
 
-	return calculate_sqrt(n, 1); /* Start the search with a guess of 1 */
+	return (calculate_sqrt(n, 1)); /* Start the search with a guess of 1 */
 }
