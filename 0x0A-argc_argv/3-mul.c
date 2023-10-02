7fcdf6a37fb830e547e30007c7487605de96de5b
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-02 14:16:00 -0700

    multiply two digits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,49 @@
  *
  * Return: 0 on success and 1 otherwise
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	if (argc == 3)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
 
-	int mul = num1 * num2;
+		int mul = num1 * num2;
+		
+		int i = 0;
+		int j;
+		char buffer[20];
 
-	_putchar(mul + '0');
-	_putchar('\n');
+		if (mul < 0)
+		{
+			_putchar('-');
+			mul = - mul;
+		}
+		
+		if (mul == 0)
+			_putchar('0');
+		while (mul > 0)
+		{
+			buffer[i++] = (mul % 10) + '0';
+			mul /= 10;
+		}
+		
+		for (j = i; j >= 0; j--)
+			_putchar(buffer[j]);
 
-	exit(EXIT_SUCCESS);
+		_putchar('\n');
+
+		exit(EXIT_SUCCESS);
+	}
+	else 
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+
+		exit(EXIT_FAILURE);
+	}
 }
