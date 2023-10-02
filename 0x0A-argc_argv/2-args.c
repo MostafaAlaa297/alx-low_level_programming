fc69b8ee4727ff9c57cca95958a24b63ccf2a041
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-02 09:16:13 -0700

    Print all recieved arguments from argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * args - Print all the arguments it receives
+ * @argc: The number of arguments
+ * @argv: An array of the arguments
+ *
+ * Return: 0 on success, and 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	while(argc--)
+	{
+		char *temp = *argv;
+		while(*temp)
+		{
+			_putchar(*temp);
+			temp++;
+		}
+		_putchar('\n');
+		argv++;
+	}
+
+	exit(EXIT_SUCCESS);
+}
