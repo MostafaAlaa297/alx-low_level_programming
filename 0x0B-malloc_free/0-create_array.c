a36d1ee6a00a380ac09803849df19b3b9cfd6c75
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-03 11:21:58 -0700

    Edit code to allocate memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * create_array - creates an array of chars,
@@ -14,10 +15,32 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc((size) * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
+
+	if (s == NULL)
+	{
+		_putchar('N');
+		_putchar('o');
+		_putchar('t');
+		_putchar(' ');
+		_putchar('e');
+		_putchar('n');
+		_putchar('o');
+		_putchar('u');
+		_putchar('g');
+		_putchar('h');
+		_putchar(' ');
+		_putchar('m');
+		_putchar('e');
+		_putchar('m');
+		_putchar('o');
+		_putchar('r');
+		_putchar('y');
+		_putchar('\n');
+	}
 	
 	i = 0;
 	while (i < size)
