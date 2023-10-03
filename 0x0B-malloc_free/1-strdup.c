56fde157b48968c2153052244f757d1b35685f76
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-03 15:08:38 -0700

    Return pointer to a string which is a dupicate to a string, using mlloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: The string to be copied
+ *
+ * Return: A pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i;
+	char *s;
+
+	while (str[size] != '\0')
+		size++;
+
+	s = malloc((size + 1) * sizeof(char));
+
+	if (s == NULL)
+        {
+            printf("Can't allocate %d bytes\n", (size + 1));
+            return (NULL);
+        }
+
+	i = 0;
+	while (i < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
