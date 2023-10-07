4874993e3139627c6d4147084c44b9d7c263f366
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-07 11:29:48 -0700

    Change malloc to calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = malloc(nmemb * size);
+	void *ptr = calloc(nmemb, size);
 	
 	if (nmemb == 0 || size == 0)
 		return NULL;
