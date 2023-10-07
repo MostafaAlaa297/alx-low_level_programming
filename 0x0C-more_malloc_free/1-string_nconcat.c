7a83b1ea45c60b794d7d30395255463e63dddeba
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-07 09:12:07 -0700

    Fix else bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,18 +28,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len2++;
 	}
 
-	if (n >= len2)
+	if (n > len2)
 		n = len2;
 
 	ptr = malloc((len1 + n) * sizeof(char));
 
 	if (ptr == NULL)
 		return NULL;
-	else
-	{
-		for (i = 0; i < len1; i++)
-			ptr[i] = s1[i];
-	}
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+
 	for (j = 0; j < n; j++)
 		ptr[i + j] = s2[j];
 
