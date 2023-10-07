b2ea7ecffbb81f4ad875bdd8a0c54d6268381955
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-07 10:53:03 -0700

    Uncomment empty string case

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,11 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return NULL;
 	if(s1 == NULL)
 		s1 = "";
-	/*if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];*/
+		ptr[i] = s1[i];
 
 	if (n >= len2)
 	{
