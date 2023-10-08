48f99568f925d817762ac646557495c08b309f96
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-07 18:31:54 -0700

    Add parenthesis to return

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,14 +11,14 @@ int *array_range(int min, int max)
 	int *ptr, i, length;
 
 	if (min > max)
-		return NULL;
+		return (NULL);
 
 	length = (max - min) + 1;
-       	
+	
 	ptr= malloc(sizeof(int) * length);
 
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < length; i++, min++)
 		ptr[i] = min;
