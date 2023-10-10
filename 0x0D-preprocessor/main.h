11cc2f75b73a45c5649c0a2954ef1295c9b3916f
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-10 15:18:08 -0700

    Create macro size

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcpy(char *dest, char *src);
+
+void print_array(int *a, int n);
+
+void puts_half(char *str);
+
+void _puts(char *str);
+
+void puts2(char *str);
+
+int _strlen(char *s);
+
+void swap_int(int *a, int *b);
+
+int _putchar(char c);
+
+void reset_to_98(int * n);
+
+void rev_string(char *s);
+
+void print_rev(char *s);
+
+#endif /* MAIN_H */
