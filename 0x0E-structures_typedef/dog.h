7b90eafacf47ac891e7a58d8e6786c9d28ca4b7d
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-10 17:34:13 -0700

    Create structure for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * strut dog - short descreption
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner's name
+ *
+ * Descrition: Long description
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_info;
