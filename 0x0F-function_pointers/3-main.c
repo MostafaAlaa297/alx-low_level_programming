75c7bde89d91f867884d115895ddd1245bd73569
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-11 18:25:26 -0700

    Create program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 for success, 98 for incorrect number of arguments, 99 for
+ * invalid operator, 100 for division by zero
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
