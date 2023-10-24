a3e881dbd36294feecef4b5a0f10957844d15c08
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-24 15:49:40 -0700

    Remove trailing white space & edit doc

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * delete_nodeint - Deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - Deletes the node at
+ * index of a listint_t linked list
  * @head: A pointer to the first element of the list
  * @index: The index of the node that should be deleted
  *
@@ -15,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	
+
 	if (index == 0)
 	{
 		temp = *head;
