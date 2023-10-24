beea3db96e92b1af36b3dc5b731df0a1540d3145
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-24 11:03:25 -0700

    Use current intead of head & remove trailing white space

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,14 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	
+	listint_t *current;
+
+	current = head;
+
 	for (i = 0; i < index; i++)
 	{
-		head = head->next;
+		current = current->next;
 	}
 
-	return (head);
+	return (current);
 }
