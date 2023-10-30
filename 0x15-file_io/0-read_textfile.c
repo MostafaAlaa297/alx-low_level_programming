de0390d33aabaec92344e4692a7d843de115aaf9
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-30 12:41:50 -0700

    Read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read a textfile and print it
+ * @filename: filename to read
+ * @letters: number of the letters
+ * Return: the actual number of letters to be printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(file_descriptor);
+	free(buffer);
+
+	return (bytes_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
