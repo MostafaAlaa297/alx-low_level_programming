5d5a40dc9c7e5b804d485a02065d961403210552
MostafaAlaa297 <elaskary609@gmail.com>
2023-12-07 09:34:11 -0800

    Edit print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,15 +13,18 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp;
 	int counter = 0;
+
 	if (h == NULL)
 		return (0);
 
 	temp = h;
 	while (temp)
 	{
-		printf("%d\n",temp->n);
+		printf("%d\n", temp->n);
 		counter++;
 		temp = temp->next;
 	}
+
 	return (counter);
+
 }
